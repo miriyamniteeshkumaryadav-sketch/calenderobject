752a0002852ae87657b77382e8e9f7a3efb38ae0
miriyamniteeshkumaryadav-sketch <miriyamniteeshkumaryadav@gmail.com>
2025-08-29 21:31:03 +0530

    year objects

---
+++ overalobjects.js
@@ -0,0 +1,144 @@
+let yearObject = {
+  event1: { date: "2025-01-01", event: "New Year Celebration" },
+  event2: { date: "2025-01-14", event: "Makar Sankranthi" },
+  event3: { date: "2025-01-26", event: "Republic Day" },
+  event4: { date: "2025-02-02", event: "Vasantha Panchami" },
+  event5: { date: "2025-02-19", event: "Shivaji Jayanti" },
+  event6: { date: "2025-02-26", event: "Shivaratri" },
+  event7: { date: "2025-03-02", event: "Ramadan Start" },
+  event8: { date: "2025-03-14", event: "Holi" },
+  event9: { date: "2025-04-06", event: "Sri Rama Navami" },
+  event10: { date: "2025-04-14", event: "Ambedkar Jayanti" },
+  event11: { date: "2025-05-09", event: "Rabindranath Tagore Birthday" },
+  event12: { date: "2025-06-07", event: "Bakrid" },
+  event13: { date: "2025-07-06", event: "Muharram" },
+  event14: { date: "2025-08-09", event: "Raksha Bandhan" },
+  event15: { date: "2025-08-15", event: "Independence Day" },
+  event16: { date: "2025-08-27", event: "Vinayaka Chavithi" },
+  event17: { date: "2025-09-05", event: "Teacher’s Day" },
+  event18: { date: "2025-10-02", event: "Gandhi Jayanti" },
+  event19: { date: "2025-10-20", event: "Deepawali" },
+  event20: { date: "2025-11-05", event: "Guru Nanak Jayanti" },
+  event21: { date: "2025-12-25", event: "Christmas" }
+};
+
+
+
+
+let monthObject = {
+  January: [
+    { date: "2025-01-01", event: "New Year Celebration" },
+    { date: "2025-01-14", event: "Makar Sankranthi" },
+    { date: "2025-01-26", event: "Republic Day" }
+  ],
+  February: [
+    { date: "2025-02-02", event: "Vasantha Panchami" },
+    { date: "2025-02-19", event: "Shivaji Jayanti" },
+    { date: "2025-02-26", event: "Shivaratri" }
+  ],
+  March: [
+    { date: "2025-03-02", event: "Ramadan Start" },
+    { date: "2025-03-14", event: "Holi" }
+  ],
+  April: [
+    { date: "2025-04-06", event: "Sri Rama Navami" },
+    { date: "2025-04-14", event: "Ambedkar Jayanti" }
+  ],
+  May: [
+    { date: "2025-05-09", event: "Rabindranath Tagore Birthday" }
+  ],
+  June: [
+    { date: "2025-06-07", event: "Bakrid" }
+  ],
+  July: [
+    { date: "2025-07-06", event: "Muharram" }
+  ],
+  August: [
+    { date: "2025-08-09", event: "Raksha Bandhan" },
+    { date: "2025-08-15", event: "Independence Day" },
+    { date: "2025-08-27", event: "Vinayaka Chavithi" }
+  ],
+  September: [
+    { date: "2025-09-05", event: "Teacher’s Day" }
+  ],
+  October: [
+    { date: "2025-10-02", event: "Gandhi Jayanti" },
+    { date: "2025-10-20", event: "Deepawali" }
+  ],
+  November: [
+    { date: "2025-11-05", event: "Guru Nanak Jayanti" }
+  ],
+  December: [
+    { date: "2025-12-25", event: "Christmas" }
+  ]
+};
+
+
+
+
+
+
+
+
+let weekObject = {
+  week1: [
+    { date: "2025-01-01", event: "New Year Celebration" },
+    { date: "2025-09-05", event: "Teacher’s Day" }
+  ],
+  week2: [
+    { date: "2025-01-14", event: "Makar Sankranthi" },
+    { date: "2025-02-02", event: "Vasantha Panchami" },
+    { date: "2025-04-06", event: "Sri Rama Navami" },
+    { date: "2025-05-09", event: "Rabindranath Tagore Birthday" },
+    { date: "2025-07-06", event: "Muharram" },
+    { date: "2025-08-09", event: "Raksha Bandhan" },
+    { date: "2025-10-02", event: "Gandhi Jayanti" },
+    { date: "2025-11-05", event: "Guru Nanak Jayanti" }
+  ],
+  week3: [
+    { date: "2025-01-26", event: "Republic Day" },
+    { date: "2025-02-19", event: "Shivaji Jayanti" },
+    { date: "2025-03-02", event: "Ramadan Start" },
+    { date: "2025-04-14", event: "Ambedkar Jayanti" },
+    { date: "2025-06-07", event: "Bakrid" },
+    { date: "2025-08-15", event: "Independence Day" },
+    { date: "2025-10-20", event: "Deepawali" },
+    { date: "2025-12-25", event: "Christmas" }
+  ],
+  week4: [
+    { date: "2025-02-26", event: "Shivaratri" },
+    { date: "2025-03-14", event: "Holi" },
+    { date: "2025-08-27", event: "Vinayaka Chavithi" }
+  ]
+};
+
+
+
+
+
+
+
+
+let dayObject = {
+  "2025-01-01": "New Year Celebration",
+  "2025-01-14": "Makar Sankranthi",
+  "2025-01-26": "Republic Day",
+  "2025-02-02": "Vasantha Panchami",
+  "2025-02-19": "Shivaji Jayanti",
+  "2025-02-26": "Shivaratri",
+  "2025-03-02": "Ramadan Start",
+  "2025-03-14": "Holi",
+  "2025-04-06": "Sri Rama Navami",
+  "2025-04-14": "Ambedkar Jayanti",
+  "2025-05-09": "Rabindranath Tagore Birthday",
+  "2025-06-07": "Bakrid",
+  "2025-07-06": "Muharram",
+  "2025-08-09": "Raksha Bandhan",
+  "2025-08-15": "Independence Day",
+  "2025-08-27": "Vinayaka Chavithi",
+  "2025-09-05": "Teacher’s Day",
+  "2025-10-02": "Gandhi Jayanti",
+  "2025-10-20": "Deepawali",
+  "2025-11-05": "Guru Nanak Jayanti",
+  "2025-12-25": "Christmas"
+};
